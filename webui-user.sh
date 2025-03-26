0ccf954545188097e8202979f8e19887284242cc
Dosion <106908523+amDosion@users.noreply.github.com>
2025-03-26 17:45:19 +0800

    Update webui-user.sh

--- webui-user.sh
+++ webui-user.sh
@@ -25,7 +25,6 @@
 #export LAUNCH_SCRIPT="launch.py"
  
 # install command for torch
-# 在 webui-user.sh 中替换 TORCH_COMMAND
 export TORCH_COMMAND="pip install torch==2.6.0+cu128 torchvision==0.22.0+cu128 --extra-index-url https://download.pytorch.org/whl/nightly/cu128"
 
 
