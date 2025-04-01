48e66f89c121547c3900ee54c1b2beab0eeb0b90
Dosion <106908523+amDosion@users.noreply.github.com>
2025-04-01 12:31:01 +0800

    Update webui-user.sh

--- webui-user.sh
+++ webui-user.sh
@@ -25,7 +25,7 @@
 #export LAUNCH_SCRIPT="launch.py"
  
 # install command for torch
-export TORCH_COMMAND="pip install torch==2.6.0 --extra-index-url https://download.pytorch.org/whl/cu126"
+3export TORCH_COMMAND="pip install torch==2.6.0 --extra-index-url https://download.pytorch.org/whl/cu126"
 
 # Requirements file to use for stable-diffusion-webui
 #export REQS_FILE="requirements_versions.txt"
